d72d24ed34e73159cfafc27fce4824d430da1d8e
liukun <451564319@qq.com>
2017-06-12 22:49:52 +0800

    first commit

---
+++ src/main/java/cn/edu/thu/tsfile/hadoop/App.java
@@ -0,0 +1,13 @@
+package cn.edu.thu.tsfile.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
