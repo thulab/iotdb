e480d0ed81d5c462f3d55784c4f84be1dc7dd2f9
jt <jt2594838@163.com>
2017-08-10 20:07:08 +0800

    add performance test

---
+++ src/test/java/cn/edu/thu/tsfiledb/auth2/PerformanceTest.java
@@ -0,0 +1,131 @@
+package cn.edu.thu.tsfiledb.auth2;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cn.edu.thu.tsfiledb.auth.AuthException;
+import cn.edu.thu.tsfiledb.auth.dao.Authorizer;
+import cn.edu.thu.tsfiledb.auth.dao.DBDao;
+import cn.edu.thu.tsfiledb.auth2.dao.AuthDao;
+import cn.edu.thu.tsfiledb.conf.TsfileDBConfig;
+import cn.edu.thu.tsfiledb.conf.TsfileDBDescriptor;
+
+public class PerformanceTest {
+	
+	private static DBDao dbdao = null;
+	private static TsfileDBConfig dbconfig = TsfileDBDescriptor.getInstance().getConfig();
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		dbconfig.derbyHome = "";
+		dbdao = new DBDao();
+		dbdao.open();
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		dbdao.close();
+	}
+
+	@Test
+	public void testCreatOld() throws AuthException {
+		for(int i = 0; i < 1000; i++) {
+			try {
+				Authorizer.deleteUser("user" + i);
+			} catch (Exception e) {
+			}
+			Authorizer.createUser("user" + i, "password" + i);
+		}
+		assertTrue(Authorizer.login("user999", "password999"));
+	}
+	
+	@Test
+	public void testCreateNew() throws cn.edu.thu.tsfiledb.auth2.exception.AuthException {
+		for(int i = 0; i < 1000; i++) {
+			try {
+				AuthDao.getInstance().deleteUser("user" + i);
+			} catch (Exception e) {
+			}	
+			AuthDao.getInstance().addUser("user" + i, "password" + i);
+		}
+		assertTrue(AuthDao.getInstance().login("user999", "password999"));
+	}
+
+	@Test
+	public void testLoginOld() throws AuthException {
+		String username = "loginTester", password = "123456";
+		try {
+			Authorizer.createUser(username, password);
+		} catch (Exception e) {
+		}
+		for(int i = 0; i < 10000; i++) {
+			assertTrue(Authorizer.login(username, password));
+		}
+	}
+	
+	@Test
+	public void testLoginNew() throws cn.edu.thu.tsfiledb.auth2.exception.AuthException {
+		String username = "loginTester", password = "123456";
+		try {
+			AuthDao.getInstance().addUser(username, password);
+		} catch (Exception e) {
+		}
+		for(int i = 0; i < 10000; i++) {
+			assertTrue(AuthDao.getInstance().login(username, password));
+		}
+	}
+	
+	@Test
+	public void testGrantOld() throws AuthException {
+		String username = "grantTester", password = "123456";
+		String nodeName = "root.a.b.c.d.e";
+		try {
+			Authorizer.createUser(username, password);
+		} catch (Exception e) {
+		}
+		
+		for(int i = 1; i < 1000; i++) {
+			try {
+				Authorizer.removePmsFromUser(username, nodeName + "." + i, i);
+			} catch (Exception e) {
+			}
+			assertTrue(Authorizer.addPmsToUser(username, nodeName + "." + i, i));
+		}
+		
+		for(int i = 1; i < 1000; i++) {
+			assertTrue(String.valueOf(i), Authorizer.checkUserPermission(username, nodeName + "." + i, i));
+		}
+	}
+	
+	@Test
+	public void testGrantNew() throws cn.edu.thu.tsfiledb.auth2.exception.AuthException {
+		String username = "grantTester", password = "123456";
+		String roleName = "grantRole", nodeName = "root.a.b.c.d.e";
+		try {
+			AuthDao.getInstance().addUser(username, password);
+		} catch (Exception e) {
+		}
+		try {
+			AuthDao.getInstance().addRole(roleName);
+		} catch (Exception e) {
+		}
+		for(int i = 1; i < 1000; i++) {
+			try {
+				AuthDao.getInstance().revokeRolePermission(roleName, i);
+			} catch (Exception e) {
+			}
+			assertTrue(AuthDao.getInstance().grantRolePermission(roleName, i));
+			try {
+				AuthDao.getInstance().revokeRoleOnPath(username, nodeName + "." + i, roleName);
+			} catch (Exception e) {
+			}
+			assertTrue(AuthDao.getInstance().grantRoleOnPath(username, nodeName + "." + i, roleName));
+		}
+		for(int i = 1; i < 1000; i++) {
+			assertTrue(AuthDao.getInstance().checkPermissionOnPath(username, nodeName + "." + i, i));
+		}
+	}
+}
