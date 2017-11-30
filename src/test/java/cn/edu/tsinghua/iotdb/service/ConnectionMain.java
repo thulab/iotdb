a9b195eb36192cf56c74c479ad9b7f2410e4e8f7
CGF <cgf1993@foxmail.com>
2017-11-30 23:28:38 +0800

    add ConnectMain test for group by

--- src/test/java/cn/edu/tsinghua/iotdb/service/ConnectionMain.java
+++ src/test/java/cn/edu/tsinghua/iotdb/service/ConnectionMain.java
@@ -16,7 +16,7 @@ public class ConnectionMain {
 
     private static final String TIMESTAMP_STR = "Time";
     private static final String d_56 = "root.performf.group_5.d_56.s_94";
-    private static final String d_66 = "root.performf.group_6.d_66_s_10";
+    private static final String d_66 = "root.performf.group_6.d_66.s_10";
 
     static String sql1 = "SELECT max_value(s_94) FROM root.performf.group_5.d_56 GROUP BY(250000ms, 1262275200000,[1262275200000,1262276200000])";
     static String sql2 = "SELECT max_value(s_10) FROM root.performf.group_6.d_66 GROUP BY(250000ms, 1262275200000,[1262425699500,1262426699500])";
@@ -31,17 +31,17 @@ public class ConnectionMain {
 
             System.out.println("test 1 ======= ");
             boolean hasResultSet = statement.execute(sql1);
-//            if (hasResultSet) {
-//                ResultSet resultSet = statement.getResultSet();
-//                int cnt = 1;
-//                while (resultSet.next()) {
-//                    String ans = resultSet.getString(TIMESTAMP_STR) + "," + resultSet.getString(max_value(d_56));
-//                    System.out.println(ans);
-//                    cnt ++;
-//                }
-//                System.out.println(cnt);
-//            }
-//            statement.close();
+            if (hasResultSet) {
+                ResultSet resultSet = statement.getResultSet();
+                int cnt = 1;
+                while (resultSet.next()) {
+                    String ans = resultSet.getString(TIMESTAMP_STR) + "," + resultSet.getString(max_value(d_56));
+                    System.out.println(ans);
+                    cnt ++;
+                }
+                System.out.println(cnt);
+            }
+            statement.close();
 
             System.out.println("test 2 ======= ");
             statement = connection.createStatement();
