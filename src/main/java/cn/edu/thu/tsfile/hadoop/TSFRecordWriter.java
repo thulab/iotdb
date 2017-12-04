3b31c96737a0e485fa870be27056f805494521d8
liukun06 <451564319@qq.com>
2017-12-04 11:22:25 +0800

    modify the code for tsfile output format

--- src/main/java/cn/edu/thu/tsfile/hadoop/TSFRecordWriter.java
+++ src/main/java/cn/edu/thu/tsfile/hadoop/TSFRecordWriter.java
@@ -15,13 +15,9 @@ import cn.edu.thu.tsfile.hadoop.io.HDFSOutputStream;
 import cn.edu.tsinghua.tsfile.common.conf.TSFileConfig;
 import cn.edu.tsinghua.tsfile.common.conf.TSFileDescriptor;
 import cn.edu.tsinghua.tsfile.common.constant.JsonFormatConstant;
-import cn.edu.tsinghua.tsfile.timeseries.write.InternalRecordWriter;
-import cn.edu.tsinghua.tsfile.timeseries.write.TSRecordWriteSupport;
-import cn.edu.tsinghua.tsfile.timeseries.write.TSRecordWriter;
-import cn.edu.tsinghua.tsfile.timeseries.write.WriteSupport;
+import cn.edu.tsinghua.tsfile.timeseries.basis.TsFile;
 import cn.edu.tsinghua.tsfile.timeseries.write.exception.InvalidJsonSchemaException;
 import cn.edu.tsinghua.tsfile.timeseries.write.exception.WriteProcessException;
-import cn.edu.tsinghua.tsfile.timeseries.write.io.TSFileIOWriter;
 import cn.edu.tsinghua.tsfile.timeseries.write.record.TSRecord;
 import cn.edu.tsinghua.tsfile.timeseries.write.schema.FileSchema;
 
@@ -29,7 +25,7 @@ public class TSFRecordWriter extends RecordWriter<NullWritable, TSRow> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TSFRecordWriter.class);
 
-	private InternalRecordWriter<TSRecord> write = null;
+	private TsFile write = null;
 
 	public TSFRecordWriter(Path path, JSONObject schema) throws InterruptedException, IOException {
 		// construct the internalrecordwriter
@@ -43,26 +39,19 @@ public class TSFRecordWriter extends RecordWriter<NullWritable, TSRow> {
 		}
 
 		HDFSOutputStream hdfsOutputStream = new HDFSOutputStream(path, new Configuration(), false);
-
-		WriteSupport<TSRecord> writeSupport = new TSRecordWriteSupport();
-
-		TSFileIOWriter tsfileWriter = new TSFileIOWriter(fileSchema, hdfsOutputStream);
-
-		TSFileConfig conf = TSFileDescriptor.getInstance().getConfig();
-
-		if (schema.has(JsonFormatConstant.ROW_GROUP_SIZE))
-			conf.groupSizeInByte = schema.getInt(JsonFormatConstant.ROW_GROUP_SIZE);
-		if (schema.has(JsonFormatConstant.PAGE_SIZE))
-			conf.pageSizeInByte = schema.getInt(JsonFormatConstant.PAGE_SIZE);
-
-		write = new TSRecordWriter(conf, tsfileWriter, writeSupport, fileSchema);
+		try {
+			write = new TsFile(hdfsOutputStream, fileSchema);
+		} catch (WriteProcessException e) {
+			e.printStackTrace();
+			throw new IOException(e.getMessage());
+		}
 	}
 
 	@Override
 	public void write(NullWritable key, TSRow value) throws IOException, InterruptedException {
 
 		try {
-			write.write(value.getRow());
+			write.writeRecord(value.getRow());
 		} catch (WriteProcessException e) {
 			e.printStackTrace();
 			LOGGER.error("Write tsfile record error, the error message is {}", e.getMessage());
@@ -76,7 +65,5 @@ public class TSFRecordWriter extends RecordWriter<NullWritable, TSRow> {
 		LOGGER.info("Close the recordwriter, the task attempt id is {}", context.getTaskAttemptID());
 		write.close();
 	}
-	
-	
 
 }
