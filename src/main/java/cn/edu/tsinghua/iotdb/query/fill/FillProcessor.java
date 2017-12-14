6a38f8ecf9b2b2e76b1a32784c45f4801e786d17
CGF <cgf1993@foxmail.com>
2017-12-14 10:16:25 +0800

    add fill read logic in RecordReader and FillProcessor

--- src/main/java/cn/edu/tsinghua/iotdb/query/fill/FillProcessor.java
+++ src/main/java/cn/edu/tsinghua/iotdb/query/fill/FillProcessor.java
@@ -29,24 +29,94 @@ public class FillProcessor {
 
     private static final Logger LOG = LoggerFactory.getLogger(FillProcessor.class);
 
-    public static boolean getFillResult(DynamicOneColumnData result, ValueReader valueReader,
-                               DynamicOneColumnData updateTrue, DynamicOneColumnData updateFalse, InsertDynamicData insertMemoryData,
-                               long beforeTime, long queryTime)
+    /**
+     * Return false if we haven't get the correct previous value before queryTime.
+     *
+     * @param result
+     * @param valueReader
+     * @param beforeTime
+     * @param queryTime
+     * @return
+     * @throws IOException
+     */
+    public static boolean getPreviousFillResult(DynamicOneColumnData result, ValueReader valueReader,
+                                                long beforeTime, long queryTime)
             throws IOException {
+
+        if (beforeTime > valueReader.getEndTime()) {
+            return false;
+        }
 
         TSDataType dataType = valueReader.getDataType();
         CompressionTypeName compressionTypeName = valueReader.compressionTypeName;
-
-        DigestVisitor digestVisitor = new DigestVisitor();
 
         long offset = valueReader.getFileOffset();
         while ((offset - valueReader.getFileOffset()) <= valueReader.totalSize) {
             ByteArrayInputStream bis = valueReader.initBAISForOnePage(offset);
             long lastAvailable = bis.available();
+
             PageReader pageReader = new PageReader(bis, compressionTypeName);
             PageHeader pageHeader = pageReader.getNextPageHeader();
 
+            long pageMinTime = pageHeader.data_page_header.min_timestamp;
+            long pageMaxTime = pageHeader.data_page_header.max_timestamp;
+
+            if (beforeTime > pageMaxTime) {
+                pageReader.skipCurrentPage();
+                offset += lastAvailable - bis.available();
+            }
+
+            InputStream page = pageReader.getNextPage();
+            offset += lastAvailable - bis.available();
+            valueReader.setDecoder(Decoder.getDecoderByType(pageHeader.getData_page_header().getEncoding(), dataType));
+            long[] timestamps = valueReader.initTimeValue(page, pageHeader.data_page_header.num_rows, false);
+            int timeIdx = 0;
+
+            switch (dataType) {
+                case INT32:
+                    while (valueReader.decoder.hasNext(page)) {
+                        long time = timestamps[timeIdx];
+                        timeIdx++;
+
+                        int v = valueReader.decoder.readInt(page);
+
+                        if (time < beforeTime) {
+                            continue;
+                        }
+                        if (time >= beforeTime && time <= queryTime) {
+                            result.setTime(0, time);
+                            result.setInt(0, v);
+                            continue;
+                        }
+                        if (time > queryTime) {
+                            return true;
+                        }
+                    }
+                    break;
+                case INT64:
+                    while (valueReader.decoder.hasNext(page)) {
+                        long time = timestamps[timeIdx];
+                        timeIdx++;
+
+                        long v = valueReader.decoder.readLong(page);
+
+                        if (time < beforeTime) {
+                            continue;
+                        }
+                        if (time >= beforeTime && time <= queryTime) {
+                            result.setTime(0, time);
+                            result.setLong(0, v);
+                            continue;
+                        }
+                        if (time > queryTime) {
+                            return true;
+                        }
+                    }
+                    break;
+            }
+
         }
+
 
 
         return false;
