257cf014cc31e6e241ffd890d4c48414afa5c817
jt2594838 <jt2594838@163.com>
2017-12-21 11:15:00 +0800

    add reopen() add block and timeOut in close

--- src/main/java/cn/edu/tsinghua/iotdb/engine/flushthread/FlushManager.java
+++ src/main/java/cn/edu/tsinghua/iotdb/engine/flushthread/FlushManager.java
@@ -2,11 +2,15 @@ package cn.edu.tsinghua.iotdb.engine.flushthread;
 
 import cn.edu.tsinghua.iotdb.conf.TsfileDBConfig;
 import cn.edu.tsinghua.iotdb.conf.TsfileDBDescriptor;
+import cn.edu.tsinghua.tsfile.common.exception.ProcessorException;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FlushManager {
+
+    private static final int EXIT_WAIT_TIME = 60 * 1000;
 
     private ExecutorService pool;
 
@@ -24,17 +28,49 @@ public class FlushManager {
     }
 
     /**
-     * Block new flush submits and exit when all RUNNING THREAD in the pool end.
+     * @throws ProcessorException if the pool is not terminated.
      */
-    public void forceClose() {
+    public void reopen() throws ProcessorException {
+        if(!pool.isTerminated())
+            throw new ProcessorException("Pool is not terminated!");
+        TsfileDBConfig config = TsfileDBDescriptor.getInstance().getConfig();
+        pool = Executors.newFixedThreadPool(config.concurrentFlushThread);
+    }
+
+    /**
+     * Refuse new flush submits and exit when all RUNNING THREAD in the pool end.
+     * @param block if set to true, this method will wait for timeOut milliseconds.
+     * @param timeOut block time out in milliseconds.
+     * @throws ProcessorException if timeOut is reached or being interrupted while waiting to exit.
+     */
+    public void forceClose(boolean block, long timeOut) throws ProcessorException {
         pool.shutdownNow();
+        if(block) {
+            try {
+                if(!pool.awaitTermination(timeOut, TimeUnit.MILLISECONDS))
+                    throw new ProcessorException("Flush thread pool doesn't exit after " + EXIT_WAIT_TIME + " ms");
+            } catch (InterruptedException e) {
+               throw new ProcessorException("Interrupted while waiting flush thread pool to exit. " + e.getMessage());
+            }
+        }
     }
 
     /**
      * Block new flush submits and exit when all RUNNING THREADS AND TASKS IN THE QUEUE end.
+     * @param block if set to true, this method will wait for timeOut milliseconds.
+     * @param timeOut block time out in milliseconds.
+     * @throws ProcessorException if timeOut is reached or being interrupted while waiting to exit.
      */
-    public void close() {
+    public void close(boolean block, long timeOut) throws ProcessorException {
         pool.shutdown();
+        if(block) {
+            try {
+                if(!pool.awaitTermination(timeOut, TimeUnit.MILLISECONDS))
+                    throw new ProcessorException("Flush thread pool doesn't exit after " + EXIT_WAIT_TIME + " ms");
+            } catch (InterruptedException e) {
+                throw new ProcessorException("Interrupted while waiting flush thread pool to exit. " + e.getMessage());
+            }
+        }
     }
 
     synchronized public void submit(Runnable task) {
