69538428e025a22a7a44d5df84ea4b86d7bc7356
liukun06 <451564319@qq.com>
2017-12-25 18:33:42 +0800

    add jdbc thread pool factory

--- src/main/java/cn/edu/tsinghua/iotdb/utils/IoTDBThreadPoolFactory.java
+++ src/main/java/cn/edu/tsinghua/iotdb/utils/IoTDBThreadPoolFactory.java
@@ -3,27 +3,40 @@ package cn.edu.tsinghua.iotdb.utils;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.thrift.server.TThreadPoolServer.Args;
 
 /**
  * This class is used to create thread pool which must contain the pool name.
+ * 
  * @author liukun
  *
  */
 public class IoTDBThreadPoolFactory {
 
 	/**
-	 * see {@link Executors#newFixedThreadPool(int, java.util.concurrent.ThreadFactory)}
-	 * @param nThreads the number of threads in the pool
-	 * @param poolName the name of thread pool
+	 * see
+	 * {@link Executors#newFixedThreadPool(int, java.util.concurrent.ThreadFactory)}
+	 * 
+	 * @param nThreads
+	 *            the number of threads in the pool
+	 * @param poolName
+	 *            the name of thread pool
 	 * @return fixed size thread pool
 	 */
 	public static ExecutorService newFixedThreadPool(int nThreads, String poolName) {
 		return Executors.newFixedThreadPool(nThreads, new IoTThreadFactory(poolName));
 	}
-	
+
 	/**
-	 * see {@link Executors#newSingleThreadExecutor(java.util.concurrent.ThreadFactory)
-	 * @param poolName the name of thread pool
+	 * see
+	 * {@link Executors#newSingleThreadExecutor(java.util.concurrent.ThreadFactory)
+	 * 
+	 * @param poolName
+	 *            the name of thread pool
 	 * @return thread pool
 	 */
 	public static ExecutorService newSingleThreadExecutor(String poolName) {
@@ -31,8 +44,11 @@ public class IoTDBThreadPoolFactory {
 	}
 
 	/**
-	 * see {@link Executors#newCachedThreadPool(java.util.concurrent.ThreadFactory)
-	 * @param poolName the name of thread pool
+	 * see
+	 * {@link Executors#newCachedThreadPool(java.util.concurrent.ThreadFactory)
+	 * 
+	 * @param poolName
+	 *            the name of thread pool
 	 * @return thread pool
 	 */
 	public static ExecutorService newCachedThreadPool(String poolName) {
@@ -40,7 +56,9 @@ public class IoTDBThreadPoolFactory {
 	}
 
 	/**
-	 * see {@link Executors#newSingleThreadExecutor(java.util.concurrent.ThreadFactory)
+	 * see
+	 * {@link Executors#newSingleThreadExecutor(java.util.concurrent.ThreadFactory)
+	 * 
 	 * @param poolName
 	 * @return scheduled thread pool
 	 */
@@ -49,13 +67,23 @@ public class IoTDBThreadPoolFactory {
 	}
 
 	/**
-	 * see {@link Executors#newScheduledThreadPool(int, java.util.concurrent.ThreadFactory)
-	 * @param corePoolSize the number of threads to keep in the pool
-	 * @param poolName the name of thread pool
+	 * see
+	 * {@link Executors#newScheduledThreadPool(int, java.util.concurrent.ThreadFactory)
+	 * 
+	 * @param corePoolSize
+	 *            the number of threads to keep in the pool
+	 * @param poolName
+	 *            the name of thread pool
 	 * @return thread pool
 	 */
 	public static ScheduledExecutorService newScheduledThreadPool(int corePoolSize, String poolName) {
 		return Executors.newScheduledThreadPool(corePoolSize, new IoTThreadFactory(poolName));
+	}
+
+	public static ExecutorService createJDBCThreadPool(Args args, String poolName) {
+		SynchronousQueue<Runnable> executorQueue = new SynchronousQueue<Runnable>();
+		return new ThreadPoolExecutor(args.minWorkerThreads, args.maxWorkerThreads, args.stopTimeoutVal,
+				args.stopTimeoutUnit, executorQueue, (ThreadFactory) new IoTThreadFactory(poolName));
 	}
 
 }
