bd3a7c04d33b1e4f44c0159b901560afefc0916f
Liliang <623651813@qq.com>
2018-01-11 21:51:59 +0800

    Apply Last Function

--- src/main/java/cn/edu/tsinghua/iotdb/monitor/StatMonitor.java
+++ src/main/java/cn/edu/tsinghua/iotdb/monitor/StatMonitor.java
@@ -7,10 +7,14 @@ import cn.edu.tsinghua.iotdb.exception.FileNodeManagerException;
 import cn.edu.tsinghua.iotdb.exception.MetadataArgsErrorException;
 import cn.edu.tsinghua.iotdb.exception.PathErrorException;
 import cn.edu.tsinghua.iotdb.metadata.MManager;
+import cn.edu.tsinghua.iotdb.query.engine.OverflowQueryEngine;
+import cn.edu.tsinghua.iotdb.query.management.ReadLockManager;
 import cn.edu.tsinghua.iotdb.utils.IoTDBThreadPoolFactory;
+import cn.edu.tsinghua.tsfile.common.utils.Pair;
 import cn.edu.tsinghua.tsfile.file.metadata.enums.TSDataType;
 import cn.edu.tsinghua.tsfile.timeseries.read.query.DynamicOneColumnData;
 import cn.edu.tsinghua.tsfile.timeseries.read.query.QueryDataSet;
+import cn.edu.tsinghua.tsfile.timeseries.read.support.Path;
 import cn.edu.tsinghua.tsfile.timeseries.read.support.RowRecord;
 import cn.edu.tsinghua.tsfile.timeseries.write.record.DataPoint;
 import cn.edu.tsinghua.tsfile.timeseries.write.record.TSRecord;
@@ -19,13 +23,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReadWriteLock;
 
 /**
  * @author liliang
@@ -118,22 +120,35 @@ public class StatMonitor {
 
     public void recovery() {
         // TODO: restore the FildeNode Manager TOTAL_POINTS statistics info
-        QueryDataSet queryDataSet = new QueryDataSet();
-        if (queryDataSet.hasNextRecord()) {
-            queryDataSet.next();
-            RowRecord rowRecord = queryDataSet.getCurrentRecord();
-            LinkedHashMap<String, DynamicOneColumnData> linkedHashMap = queryDataSet.mapRet;
-            FileNodeManager fManager = FileNodeManager.getInstance();
-            HashMap<String, AtomicLong> statParamsHashMap = fManager.getStatParamsHashMap();
-            for (Map.Entry<String, DynamicOneColumnData> entry : linkedHashMap.entrySet()) {
-                String[] statMeasurements = entry.getKey().split("\\.");
-                String statMeasurement = statMeasurements[statMeasurements.length-1];
-                if (statParamsHashMap.containsKey(statMeasurement)) {
-                    DynamicOneColumnData dynamicOneColumnData = entry.getValue();
-                    long lastValue = dynamicOneColumnData.getLong(dynamicOneColumnData.valueLength-1);
-                    statParamsHashMap.put(statMeasurement, new AtomicLong(lastValue));
+        OverflowQueryEngine overflowQueryEngine = new OverflowQueryEngine();
+        List<Pair<Path, String>> pairList = new ArrayList<>();
+        List<String> stringList = FileNodeManager.getInstance().getAllPathForStatistic();
+        for (String string : stringList) {
+            Path path = new Path(string);
+            pairList.add(new Pair<>(path, "last"));
+        }
+        try {
+            QueryDataSet queryDataSet;
+            queryDataSet = overflowQueryEngine.aggregate(pairList, null);
+            ReadLockManager.getInstance().unlockForOneRequest();
+            if (queryDataSet.hasNextRecord()) {
+                queryDataSet.next();
+                RowRecord rowRecord = queryDataSet.getCurrentRecord();
+                LinkedHashMap<String, DynamicOneColumnData> linkedHashMap = queryDataSet.mapRet;
+                FileNodeManager fManager = FileNodeManager.getInstance();
+                HashMap<String, AtomicLong> statParamsHashMap = fManager.getStatParamsHashMap();
+                for (Map.Entry<String, DynamicOneColumnData> entry : linkedHashMap.entrySet()) {
+                    String[] statMeasurements = entry.getKey().split("\\.");
+                    String statMeasurement = statMeasurements[statMeasurements.length-1];
+                    if (statParamsHashMap.containsKey(statMeasurement)) {
+                        DynamicOneColumnData dynamicOneColumnData = entry.getValue();
+                        long lastValue = dynamicOneColumnData.getLong(dynamicOneColumnData.valueLength-1);
+                        statParamsHashMap.put(statMeasurement, new AtomicLong(lastValue));
+                    }
                 }
             }
+        } catch (Exception e) {
+            LOGGER.error("Error occurs when recovering statistics of FileNodeManager,",e);
         }
     }
 
