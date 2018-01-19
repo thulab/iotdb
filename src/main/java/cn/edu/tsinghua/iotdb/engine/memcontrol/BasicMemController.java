7acbb9a5714d34639bbd742cef8ecad1269dc728
jt <jt2594838@163.com>
2018-01-19 11:10:22 +0800

    MemController implements IService add start and end logs of MemController

--- src/main/java/cn/edu/tsinghua/iotdb/engine/memcontrol/BasicMemController.java
+++ src/main/java/cn/edu/tsinghua/iotdb/engine/memcontrol/BasicMemController.java
@@ -2,8 +2,45 @@ package cn.edu.tsinghua.iotdb.engine.memcontrol;
 
 import cn.edu.tsinghua.iotdb.conf.TsfileDBConfig;
 import cn.edu.tsinghua.iotdb.conf.TsfileDBDescriptor;
+import cn.edu.tsinghua.iotdb.service2.IService;
+import cn.edu.tsinghua.iotdb.service2.ServiceType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public abstract class BasicMemController {
+public abstract class BasicMemController implements IService{
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicMemController.class);
+    private TsfileDBConfig config;
+
+    @Override
+    public void start() {
+        if(config.enableMemMonitor) {
+            if(monitorThread == null) {
+                monitorThread = new MemMonitorThread(config);
+                monitorThread.start();
+            } else {
+                logger.error("Attempt to start MemController but it is already started");
+            }
+            if(memStatisticThread == null) {
+                memStatisticThread = new MemStatisticThread();
+                memStatisticThread.start();
+            } else {
+                logger.error("Attempt to start MemController but it is already started");
+            }
+        }
+        logger.info("MemController starts");
+    }
+
+    @Override
+    public void stop() {
+        clear();
+        close();
+    }
+
+    @Override
+    public ServiceType getID() {
+        return null;
+    }
 
     public enum CONTROLLER_TYPE {
         RECORD, JVM
@@ -20,14 +57,9 @@ public abstract class BasicMemController {
     }
 
     BasicMemController(TsfileDBConfig config) {
+        this.config = config;
         warningThreshold = config.memThresholdWarning;
         dangerouseThreshold = config.memThresholdDangerous;
-        if(config.enableMemMonitor) {
-            monitorThread = new MemMonitorThread(config);
-            monitorThread.start();
-            memStatisticThread = new MemStatisticThread();
-            memStatisticThread.start();
-        }
     }
 
     // change instance here
@@ -61,8 +93,21 @@ public abstract class BasicMemController {
     public abstract void clear();
 
     public void close() {
-        monitorThread.interrupt();
-        memStatisticThread.interrupt();
+        logger.info("MemController exiting");
+        if(monitorThread != null) {
+            monitorThread.interrupt();
+            while (monitorThread.isAlive()) {
+            }
+            monitorThread = null;
+        }
+
+        if(memStatisticThread != null) {
+            memStatisticThread.interrupt();
+            while (memStatisticThread.isAlive()) {
+            }
+            memStatisticThread = null;
+        }
+        logger.info("MemController exited");
     }
 
     public abstract UsageLevel reportUse(Object user, long usage);
