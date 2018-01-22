854e6c9a7eb99b0b094ccb0645775b9a47e13170
XuYi <xuyithuss@126.com>
2018-01-22 21:30:05 +0800

    add comment for IService

--- src/main/java/cn/edu/tsinghua/iotdb/service/IService.java
+++ src/main/java/cn/edu/tsinghua/iotdb/service/IService.java
@@ -1,7 +1,20 @@
 package cn.edu.tsinghua.iotdb.service;
 
 public interface IService {
+	/**
+	 * Start current service.
+	 */
 	void start();
+
+	/**
+	 * Stop current service.
+	 * If current service uses thread or thread pool,
+	 * current service should guarantee to release thread or thread pool.
+	 */
 	void stop();
+
+	/**
+	 * @return current service name
+	 */
 	ServiceType getID();
 }
