c42e024f9a59a8170d9ff327f27c0df5f9c7e65e
JingYi Wang <cherishW26@163.com>
2018-01-29 17:21:09 +0800

    fix a bug

--- src/main/java/cn/edu/tsinghua/iotdb/exception/IoTDBException.java
+++ src/main/java/cn/edu/tsinghua/iotdb/exception/IoTDBException.java
@@ -5,21 +5,13 @@ public class IoTDBException extends Exception{
     public int errorCode;
     public String errorEnum;
     public String description;
-    
-<<<<<<< HEAD
 
     public IoTDBException(int errCode, String errEnum, String desc){
         super("[Error: "+errCode+"] "+errEnum+" "+ desc);
         this.errorCode=errCode;
         this.errorEnum=errEnum;
         this.description=desc;
-=======
-    public IoTDBException(int errCode, String errEnum, String desc){
-        super("[Error: "+errCode+"] "+errEnum+" "+ desc);
-        this.errorCode = errCode;
-        this.errorEnum = errEnum;
-        this.description = desc;
->>>>>>> 76b995c855873411fb1bdeb0efb976a9218b4bf0
     }
+
 
 }
