d9616b0ac88ab40468489e08738b7deff4b539c8
MyXOF <xuyi556677@163.com>
2018-03-20 19:55:02 +0800

    add wmiv to environment variable

--- iotdb/bin/stop-server.bat
+++ iotdb/bin/stop-server.bat
@@ -1,4 +1,6 @@
 @echo off
 
+setx WMIC_HOME "c:\windows\system32\wbem" /m
+
 wmic process where (commandline like "%%iotdb.IoTDB%%" and not name="wmic.exe") delete
 rem ps ax | grep -i 'iotdb.IoTDB' | grep -v grep | awk '{print $1}' | xargs kill -SIGTERM
