46e4d3ba3545046480243b771ca9d6d4f8a89b77
qiaojialin <13520302880@163.com>
2018-03-30 10:42:04 +0800

    v0.5.0 (#53)

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
     <dependency>
       <groupId>cn.edu.tsinghua</groupId>
       <artifactId>iotdb-jdbc</artifactId>
-      <version>0.4.0</version>
+      <version>0.5.0</version>
     </dependency>
 </dependencies>
 ```
--- src/main/java/cn/edu/tsinghua/iotdb/jdbc/TsFileDBConstant.java
+++ src/main/java/cn/edu/tsinghua/iotdb/jdbc/TsFileDBConstant.java
@@ -3,5 +3,5 @@ package cn.edu.tsinghua.iotdb.jdbc;
 public class TsFileDBConstant {
 
 	public static final String GLOBAL_DB_NAME = "IoTDB";
-	public static final String GLOBAL_DB_VERSION = "0.4.0";
+	public static final String GLOBAL_DB_VERSION = "0.5.0";
 }
