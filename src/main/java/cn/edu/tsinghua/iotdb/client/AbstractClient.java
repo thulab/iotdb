01008ffc707b97e127586998ea6e1db21e32721b
XuYi <MyXOF@users.noreply.github.com>
2018-05-06 09:55:52 +0800

    Update AbstractClient.java

--- src/main/java/cn/edu/tsinghua/iotdb/client/AbstractClient.java
+++ src/main/java/cn/edu/tsinghua/iotdb/client/AbstractClient.java
@@ -451,7 +451,6 @@ public abstract class AbstractClient {
 			}
 			System.out.println("Execute successfully.");
 		} catch (Exception e) {
-			e.printStackTrace();
 			System.out.println("Msg: " + e.getMessage());
 		} finally {
 			if (statement != null) {
