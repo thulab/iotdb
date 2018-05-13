a8a692f6ca96bf469a944127cdccfd20af945d30
liukun <451564319@qq.com>
2018-05-13 10:10:52 +0800

    add the performance test for merge write using series writer

--- src/main/java/cn/edu/tsinghua/iotdb/performance/MergeWriteWithSeriesWriterPerfTest.java
+++ src/main/java/cn/edu/tsinghua/iotdb/performance/MergeWriteWithSeriesWriterPerfTest.java
@@ -15,11 +15,14 @@ import cn.edu.tsinghua.tsfile.file.metadata.enums.TSEncoding;
 import cn.edu.tsinghua.tsfile.timeseries.write.desc.MeasurementDescriptor;
 import cn.edu.tsinghua.tsfile.timeseries.write.exception.WriteProcessException;
 import cn.edu.tsinghua.tsfile.timeseries.write.io.TsFileIOWriter;
+import cn.edu.tsinghua.tsfile.timeseries.write.page.IPageWriter;
+import cn.edu.tsinghua.tsfile.timeseries.write.page.PageWriterImpl;
 import cn.edu.tsinghua.tsfile.timeseries.write.record.DataPoint;
 import cn.edu.tsinghua.tsfile.timeseries.write.record.TSRecord;
 import cn.edu.tsinghua.tsfile.timeseries.write.schema.FileSchema;
+import cn.edu.tsinghua.tsfile.timeseries.write.series.SeriesWriterImpl;
 
-public class MergeWritePerfCompaeTest {
+public class MergeWriteWithSeriesWriterPerfTest {
 
 	private static final String outPutFilePath = "output";
 	private static long memThreshold = TSFileDescriptor.getInstance().getConfig().groupSizeInByte;
@@ -30,48 +33,60 @@ public class MergeWritePerfCompaeTest {
 
 	public static void main(String[] args) throws WriteProcessException, IOException {
 		writeMultiSeriesTest();
-//		writeSingleSeriesTest();
+		// writeSingleSeriesTest();
 	}
 
 	private static void writeMultiSeriesTest() throws WriteProcessException, IOException {
 		FileSchema fileSchema = new FileSchema();
 		File outPutFile = new File(outPutFilePath);
 		outPutFile.delete();
-		for (int i = 1; i <= 500; i++)
+		for (int i = 1; i <= 1000; i++)
 			fileSchema.registerMeasurement(new MeasurementDescriptor("s" + i, TSDataType.INT32, TSEncoding.RLE));
 		// TsFileWriter fileWriter = new TsFileWriter(outPutFile, fileSchema,
 		// TSFileDescriptor.getInstance().getConfig());
 		writer = new TsFileIOWriter(outPutFile);
 		memSize.set(0);
-		workMemTable = new PrimitiveMemTable();
 		long startTime = System.currentTimeMillis();
-		for (int i = 1; i <= 1000; i++) {
-			for (int j = 1; j <= 500; j++) {
-				for (long time = 1; time <= 2257; time++) {
-					TSRecord record = new TSRecord(time, "d" + i);
-					record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, "s" + j,
-							String.valueOf(1000 + random.nextInt(100))));
-					workMemTable.write(record.deltaObjectId, record.dataPointList.get(0).getMeasurementId(),
-							record.dataPointList.get(0).getType(), record.time,
-							record.dataPointList.get(0).getValue().toString());
-
-					long mem = memSize.addAndGet(MemUtils.getRecordSize(record));
-					// System.out.println(MemUtils.bytesCntToStr(mem));
-					if (mem > memThreshold) {
-						long flushStartTime = System.currentTimeMillis();
-						long posStart = writer.getPos();
-						MemTableFlushUtil.flushMemTable(fileSchema, writer, workMemTable);
-						workMemTable = new PrimitiveMemTable();
-						memSize.set(0);
-						System.out.println(System.currentTimeMillis() - flushStartTime + " size: "
-								+ MemUtils.bytesCntToStr(writer.getPos() - posStart));
+		long disksize = 0;
+		for (int i = 1; i <= 500; i++) {
+			String deltaObjectId = "d" + i;
+			long recordCount = 0;
+			long startPos = writer.getPos();
+			writer.startRowGroup(deltaObjectId);
+			for (int j = 1; j <= 100; j++) {
+				String measurementId = "s" + j;
+				MeasurementDescriptor desc = fileSchema.getMeasurementDescriptor(measurementId);
+				IPageWriter pageWriter = new PageWriterImpl(desc);
+				SeriesWriterImpl seriesWriter = new SeriesWriterImpl(deltaObjectId, desc, pageWriter, 64 * 1024);
+				TSDataType dataType = desc.getType();
+				for (long time = 1; time <= 2257*10; time++) {
+					recordCount++;
+					switch (dataType) {
+					case INT64:
+						break;
+					case FLOAT:
+						break;
+					case DOUBLE:
+						break;
+					case TEXT:
+						break;
+					case INT32:
+						seriesWriter.write(time, 1000 + random.nextInt(100));
+						break;
+					default:
+						break;
 					}
 				}
+				seriesWriter.writeToFileWriter(writer);
 			}
+			long memSize = writer.getPos() - startPos;
+			disksize += memSize;
+			writer.endRowGroup(memSize, recordCount);
 		}
 		writer.endFile(fileSchema);
 		long endTime = System.currentTimeMillis();
 		System.out.println(String.format("write multi series time cost %dms", endTime - startTime));
+		System.out.println(String.format("file size is %s", MemUtils.bytesCntToStr(disksize)));
 	}
 
 	private static void writeSingleSeriesTest() throws WriteProcessException, IOException {
