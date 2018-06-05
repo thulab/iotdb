27b9a5320eaa13ec565df16d615761ecfd4bea89
East <952945925@qq.com>
2018-06-05 22:27:43 +0800

    test PathUIDManager

--- src/main/java/cn/edu/tsinghua/iotdb/engine/PathUIDManager.java
+++ src/main/java/cn/edu/tsinghua/iotdb/engine/PathUIDManager.java
@@ -11,6 +11,7 @@ import cn.edu.tsinghua.tsfile.format.PageHeader;
 import cn.edu.tsinghua.tsfile.timeseries.read.TsRandomAccessLocalFileReader;
 import cn.edu.tsinghua.tsfile.timeseries.readV2.reader.impl.PageReader;
 import cn.edu.tsinghua.tsfile.timeseries.write.io.TsFileIOWriter;
+import com.sun.org.apache.bcel.internal.generic.FLOAD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -104,13 +105,32 @@ public class PathUIDManager {
         return UID;
     }
 
+    public long getUID(String path){
+        if(path.startsWith(ROOT))path = path.substring((ROOT + PATH_SEPARATOR).length() - 1);
+        List<String> subPathList = Arrays.asList(path.split(PATH_SEPARATOR));
+        if(subPathList.size() != size){
+            LOGGER.error("wrong subpath num of path %s.", path);
+        }
+
+        long UID = 0;
+        for(int i = 0;i < size;i++){
+            long subUID = pathList.get(i).indexOf(subPathList.get(i));
+            if(subUID == -1){
+                LOGGER.warn("can not recognize path %s", path);
+                return  -1;
+            }
+            UID += subUID * shiftList.get(i);
+        }
+        return UID;
+    }
+
     private long getSubUID(long UID, int index){
         long subUID = UID & maskList.get(index);
         int shift = 0;
         for(int i = index + 1;i < size;i++){
             shift += lengthList.get(i);
         }
-        subUID = subUID >> shift;
+        subUID = subUID >> (shift * 4);
         return subUID;
     }
 
@@ -124,12 +144,14 @@ public class PathUIDManager {
     }
 
     public static void main(String[] args) throws IOException {
+        long starttime, endtime, time;
+
         System.out.println("start read file");
         List<String> pathList = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader("/Users/East/Desktop/path_uid/path.txt"));
         String str = null;
 //        while((str = br.readLine()) != null) {
-        for(int i = 0;i < 10000;i++){
+        for(int i = 0;i < 1000000;i++){
             str = br.readLine();
             pathList.add(str);
         }
@@ -137,64 +159,77 @@ public class PathUIDManager {
         System.out.println("complete read file");
 
         System.out.println("start init");
-        Map<String, Integer> pathMap1 = new HashMap<>();
-        Map<Long, Integer> pathMap2 = new HashMap<>();
+        starttime = System.currentTimeMillis();
         List<Long> UIDList = new ArrayList<>();
         PathUIDManager manager = new PathUIDManager();
         for(int i = 0;i < pathList.size();i++) {
-            pathMap1.put(pathList.get(i), i);
             long UID = manager.addPath(pathList.get(i));
             UIDList.add(UID);
-            pathMap2.put(UID, i);
-            System.out.println("init:" + (i / (float)pathList.size()) + "\t" + i);
+//            System.out.println("init:" + (i / (float)pathList.size()) + "\t" + i);
+            if(i % 1000000 == 0){
+                endtime = System.currentTimeMillis();
+                System.out.println(i + ":" + (endtime - starttime));
+                starttime = System.currentTimeMillis();
+            }
         }
         System.out.println("complete init");
 
         System.out.println("start prove correctness");
         int wrongnum = 0;
         for(int i = 0;i < pathList.size();i++){
-            System.out.println(i);
-            System.out.println(Long.toHexString(UIDList.get(i)));
             if(!manager.getPath(UIDList.get(i)).equals(pathList.get(i)))wrongnum++;
+
+            if(manager.getUID(pathList.get(i)) != UIDList.get(i))wrongnum++;
         }
         System.out.println("wrong num:" + wrongnum);
         System.out.println("end prove correctness");
 
         System.out.println("start query");
+        Map<String, Integer> pathMap1 = new HashMap<>();
+        Map<Long, Integer> pathMap2 = new HashMap<>();
+        for(int i = 0;i < pathList.size();i++){
+//            System.out.println(i / (float)pathList.size());
+            String path = pathList.get(i);
+            pathMap1.put(path, i);
+            pathMap2.put(manager.getUID(path), i);
+        }
         Random random = new Random(System.currentTimeMillis());
         long stime, ltime;
         stime = ltime = 0;
-        long starttime, endtime, time;
-        for(int i = 0;i < 100;i++){
-            int index = random.nextInt(pathList.size());
-            String path = pathList.get(index);
-            long UID = UIDList.get(index);
-            int value1, value2;
-            value1 = 1;
-            value2 = 2;
+        wrongnum = 0;
+        for(int k = 0;k < 3;k++) {
+            int looptime = (int) Math.pow(10, k);
+            for (int i = 0; i < looptime; i++) {
+                int index = random.nextInt(pathList.size());
+                String path = pathList.get(index);
+                int value1 = 1;
+                int value2 = 2;
+                long UID;
 
-            starttime = System.currentTimeMillis();
-            for(int j = 0;j < 1000000;j++) {
-                value1 = pathMap1.get(path);
+                starttime = System.currentTimeMillis();
+                for (int j = 0; j < 1000000; j++) {
+                    value1 = pathMap1.get(path);
+                }
+                endtime = System.currentTimeMillis();
+                time = endtime - starttime;
+                stime += time;
+
+                starttime = System.currentTimeMillis();
+                for (int j = 0; j < 1000000; j++) {
+                    UID = manager.getUID(path);
+                    value2 = pathMap2.get(UID);
+                }
+                endtime = System.currentTimeMillis();
+                time = endtime - starttime;
+                ltime += time;
+
+                if(value1 != value2)wrongnum++;
             }
-            endtime = System.currentTimeMillis();
-            time = endtime - starttime;
-            stime += time;
-            System.out.println(time);
-
-            starttime = System.currentTimeMillis();
-            for(int j = 0;j < 1000000;j++) {
-                value2 = pathMap2.get(UID);
-            }
-            endtime = System.currentTimeMillis();
-            time = endtime - starttime;
-            ltime += time;
-            System.out.println(time);
-
-            System.out.println(value1 == value2);
+            System.out.println("looptime:" + looptime);
+            System.out.println(stime);
+            System.out.println(ltime);
+            System.out.println("wrong num:" + wrongnum);
         }
-        System.out.println(stime);
-        System.out.println(ltime);
         System.out.println("end query");
     }
 }
