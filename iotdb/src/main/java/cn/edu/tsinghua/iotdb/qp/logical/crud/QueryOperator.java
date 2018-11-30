ca8134ac6b12ee8e84d50c7de16aa8a804dce1ee
RuiLei <ruileisurprise@gmail.com>
2018-11-30 14:44:31 +0800

    adapt Slimit2Server to the new integeral version of IoTDB

--- iotdb/src/main/java/cn/edu/tsinghua/iotdb/qp/logical/crud/QueryOperator.java
+++ iotdb/src/main/java/cn/edu/tsinghua/iotdb/qp/logical/crud/QueryOperator.java
@@ -27,6 +27,10 @@ public class QueryOperator extends SFWOperator {
     private Map<TSDataType, IFill> fillTypes;
     private boolean isFill = false;
 
+    private int seriesLimit;
+    private int seriesOffset;
+    private boolean isSlimit = false; // false if sql does not contain SLIMIT clause
+
     public boolean isFill() {
         return isFill;
     }
@@ -49,6 +53,31 @@ public class QueryOperator extends SFWOperator {
 
     public boolean isGroupBy() {
         return isGroupBy;
+    }
+
+    public void setSlimit(int seriesLimit) {
+        this.seriesLimit = seriesLimit;
+        this.isSlimit = true;
+    }
+
+    public void setSoffset(int seriesOffset) {
+        /*
+         Since soffset cannot be set alone without slimit,
+         `isSlimit` only need to be set true in the `setSlimit` function.
+         */
+        this.seriesOffset = seriesOffset;
+    }
+
+    public int getSeriesLimit() {
+        return seriesLimit;
+    }
+
+    public int getSeriesOffset() {
+        return seriesOffset;
+    }
+
+    public boolean isSlimit() {
+        return isSlimit;
     }
 
     public long getUnit() {
