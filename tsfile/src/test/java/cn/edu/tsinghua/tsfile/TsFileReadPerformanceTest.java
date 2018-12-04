b57d3dca4bb4a2a5b1ea3d86ed0d624e3adbfd2a
CGF <cgf1993@foxmail.com>
2018-12-04 10:05:34 +0800

    add test with filter in TsFilePerformanceTest

--- tsfile/src/test/java/cn/edu/tsinghua/tsfile/TsFileReadPerformanceTest.java
+++ tsfile/src/test/java/cn/edu/tsinghua/tsfile/TsFileReadPerformanceTest.java
@@ -1,14 +1,20 @@
 package cn.edu.tsinghua.tsfile;
 
 import cn.edu.tsinghua.tsfile.file.metadata.enums.TSDataType;
+import cn.edu.tsinghua.tsfile.timeseries.filter.ValueFilter;
+import cn.edu.tsinghua.tsfile.timeseries.filter.expression.QueryFilter;
+import cn.edu.tsinghua.tsfile.timeseries.filter.expression.impl.SeriesFilter;
 import cn.edu.tsinghua.tsfile.timeseries.read.TsFileSequenceReader;
 import cn.edu.tsinghua.tsfile.timeseries.read.basis.ReadOnlyTsFile;
 import cn.edu.tsinghua.tsfile.timeseries.read.common.Path;
+import cn.edu.tsinghua.tsfile.timeseries.read.datatype.RowRecord;
 import cn.edu.tsinghua.tsfile.timeseries.read.datatype.RowRecordV2;
 import cn.edu.tsinghua.tsfile.timeseries.read.datatype.TimeValuePair;
 import cn.edu.tsinghua.tsfile.timeseries.read.datatype.TsPrimitiveType;
 import cn.edu.tsinghua.tsfile.timeseries.read.query.QueryExpression;
+import cn.edu.tsinghua.tsfile.timeseries.read.query.dataset.DataSetWithTimeGenerator;
 import cn.edu.tsinghua.tsfile.timeseries.read.query.dataset.DataSetWithoutTimeGenerator;
+import cn.edu.tsinghua.tsfile.timeseries.read.query.dataset.QueryDataSet;
 import cn.edu.tsinghua.tsfile.timeseries.read.reader.DynamicOneColumnData;
 
 import java.io.IOException;
@@ -24,7 +30,9 @@ public class TsFileReadPerformanceTest {
 
         //TimeUnit.SECONDS.sleep(10);
 
-        readTestV8WithoutFilter();
+        //readTestV8WithoutFilter();
+
+        readTestWithFilter();
 
     }
 
@@ -44,6 +52,35 @@ public class TsFileReadPerformanceTest {
         }
         endTime = System.currentTimeMillis();
         System.out.println(String.format("after, consume time : %sms", endTime - startTime));
+    }
+
+    private static void readTestWithFilter() throws IOException {
+        TsFileSequenceReader reader = new TsFileSequenceReader(filePath);
+        ReadOnlyTsFile tsFile = new ReadOnlyTsFile(reader);
+        QueryExpression queryExpression = QueryExpression.create();
+        for (int i = 1; i <= deviceNum; i++) {
+            for (int j = 0; j < sensorNum; j++) {
+                queryExpression.addSelectedPath(new Path(getPerformPath(i, j)));
+            }
+        }
+
+        QueryFilter valueFilter = new SeriesFilter(new Path("d1.s2"), ValueFilter.ltEq(80.0f));
+        queryExpression.setQueryFilter(valueFilter);
+
+        long startTime = System.currentTimeMillis();
+        int cnt = 0;
+        QueryDataSet queryDataSet = tsFile.query(queryExpression);
+        while (queryDataSet.hasNext()) {
+            RowRecord record = queryDataSet.next();
+//            System.out.println(record.toString());
+//            if (cnt % 5000 == 0) {
+//                System.out.println(record.toString());
+//            }
+            cnt++;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("consume time : %sms, row count : %s", endTime - startTime, cnt));
+        tsFile.close();
     }
 
     private static void readTestV8WithoutFilter() throws IOException {
