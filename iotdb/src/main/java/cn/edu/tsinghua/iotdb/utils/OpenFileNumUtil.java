e2e2ceb4d7ebdc5cd2fb5be6585374bd8245b36f
liuruiyiyang <245422695@qq.com>
2018-12-14 15:52:28 +0800

    import particular class instead of util.*

--- iotdb/src/main/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtil.java
+++ iotdb/src/main/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtil.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.EnumMap;
 
 /**
  * @author liurui
