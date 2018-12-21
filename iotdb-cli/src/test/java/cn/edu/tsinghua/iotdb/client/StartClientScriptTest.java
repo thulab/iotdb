12149b9c4499882d109671f8f9e9d24afc33b788
MyXOF <xuyi556677@163.com>
2018-12-21 15:25:18 +0800

    add mac script test for cli

--- iotdb-cli/src/test/java/cn/edu/tsinghua/iotdb/client/StartClientScriptTest.java
+++ iotdb-cli/src/test/java/cn/edu/tsinghua/iotdb/client/StartClientScriptTest.java
@@ -12,12 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StartClientScriptTest {
-	private final String[] output = {
-			"````````````````````````",
-			"Starting IoTDB Client",
-			"````````````````````````",
-			"IoTDB> Connection Error, please check whether the network is avaliable or the server has started.. Host is 127.0.0.1, port is 6667."
-		};
+
 
 	@Before
 	public void setUp() throws Exception {
@@ -29,53 +24,66 @@ public class StartClientScriptTest {
 
 	@Test
 	public void test() throws IOException, InterruptedException {
-        String os = System.getProperty("os.name").toLowerCase();
-        if(os.startsWith("windows")){
-    		testStartClientOnWindows();
-        } else {
-        	testStartClientOnUnix();
-        }
-	}
-	
-	private void testStartClientOnWindows() throws IOException{
-		String dir = getCurrentPathOnWinddows();
-		ProcessBuilder builder = new ProcessBuilder("cmd.exe", 
-				"/c", 
-				dir+File.separator+"cli"+File.separator+"bin"+File.separator+"start-client.bat",
-				"-h",
-				"xxx.xxx.xxx.xxx",
-				"-p",
-				"xxxx",
-				"-u",
-				"root",
-				"-pw",
-				"root");
-        builder.redirectErrorStream(true);
-        Process p = builder.start();
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        int i = 0;
-        while (true) {
-            line = r.readLine();
-            if (line == null) { 
-            	break; 
-            }
-            assertEquals(output[i], line);
-            i++;
-        }
-	}
-	
-	private void testStartClientOnUnix(){
-		
-	}
-	
-	private String getCurrentPathOnWinddows() throws IOException{
-		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "echo %cd%");
-	    builder.redirectErrorStream(true);
-	    Process p = builder.start();
-	    BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	    String path = r.readLine();
-	    return path;
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.startsWith("windows")) {
+			testStartClientOnWindows();
+		} else {
+			testStartClientOnUnix();
+		}
 	}
 
+	private void testStartClientOnWindows() throws IOException {
+		final String[] output = { 
+				"````````````````````````", 
+				"Starting IoTDB Client", "````````````````````````",
+				"IoTDB> Connection Error, please check whether the network is avaliable or the server has started.. Host is 127.0.0.1, port is 6668." 
+			};
+		String dir = getCurrentPath("cmd.exe", "/c", "echo %cd%");
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+				dir + File.separator + "cli" + File.separator + "bin" + File.separator + "start-client.bat", "-h",
+				"127.0.0.1", "-p", "6668", "-u", "root", "-pw", "root");
+		testOutput(builder, output);
+	}
+	
+	private void testStartClientOnUnix() throws IOException {
+		final String[] output = { 
+				"---------------------", 
+				"Starting IoTDB Client", 
+				"---------------------",
+				"IoTDB> Connection Error, please check whether the network is avaliable or the server has started.. Host is 127.0.0.1, port is 6668." 
+			};
+		String dir = getCurrentPath("pwd");
+		System.out.println(dir);
+		ProcessBuilder builder = new ProcessBuilder("sh",
+				dir + File.separator + "cli" + File.separator + "bin" + File.separator + "start-client.sh", "-h",
+				"127.0.0.1", "-p", "6668", "-u", "root", "-pw", "root");
+		testOutput(builder, output);
+	}
+	
+	private void testOutput(ProcessBuilder builder, String[] output) throws IOException {
+		builder.redirectErrorStream(true);
+		Process p = builder.start();
+		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line;
+		int i = 0;
+		while (true) {
+			line = r.readLine();
+			if (line == null) {
+				break;
+			}
+			assertEquals(output[i], line);
+			i++;
+		}
+		r.close();
+		p.destroy();
+	}
+
+	private String getCurrentPath(String...command) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(command);
+		builder.redirectErrorStream(true);
+		Process p = builder.start();
+		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String path = r.readLine();
+		return path;
+	}
 }
