a79fed5efbef3c6b7bb3fb85afa62be01dd84282
CGF <cgf1993@foxmail.com>
2018-12-24 13:47:07 +0800

    perfect test

--- iotdb/src/main/java/cn/edu/tsinghua/iotdb/PerformanceTest.java
+++ iotdb/src/main/java/cn/edu/tsinghua/iotdb/PerformanceTest.java
@@ -19,15 +19,50 @@ import java.util.List;
  */
 public class PerformanceTest {
 
-    private static int deviceStart = 1, deviceEnd = 1;
-    private static int sensorStart = 1, sensorEnd = 1;
+    private static int deviceStart = 9, deviceEnd = 9;
+    private static int sensorStart = 10, sensorEnd = 10;
 
     public static void main(String[] args) throws IOException, FileNodeManagerException {
-        //IoTDB.getInstance().active();
-        queryWithoutFilterTest();
+        //queryWithoutFilterTest();
+        singleWithoutFilterTest();
     }
 
     public static void queryWithoutFilterTest() throws IOException, FileNodeManagerException {
+
+        for (int start1 = 10; start1 >= 0; start1--) {
+            for (int start2 = 10; start2 >= 10; start2--) {
+
+                List<Path> selectedPathList = new ArrayList<>();
+                for (int i = start1; i <= deviceEnd; i++) {
+                    for (int j = start2; j <= sensorEnd; j++) {
+                        selectedPathList.add(getPath(i, j));
+                    }
+                }
+
+                QueryExpression queryExpression = QueryExpression.create(selectedPathList, null);
+
+                EngineQueryRouter queryRouter = new EngineQueryRouter();
+
+                long startTime = System.currentTimeMillis();
+
+                QueryDataSet queryDataSet = queryRouter.query(queryExpression);
+
+                int count = 0;
+                while (queryDataSet.hasNext()) {
+                    RowRecord rowRecord = queryDataSet.next();
+                    count++;
+//            if (count % 10000 == 0) {
+//                System.out.println(rowRecord);
+//            }
+                }
+
+                long endTime = System.currentTimeMillis();
+                System.out.println(String.format("start1:%s, start2:%s. Time consume : %s, count number : %s", start1, start2, endTime - startTime, count));
+            }
+        }
+    }
+
+    public static void singleWithoutFilterTest() throws IOException, FileNodeManagerException {
 
         List<Path> selectedPathList = new ArrayList<>();
         for (int i = deviceStart; i <= deviceEnd; i++) {
@@ -48,13 +83,26 @@ public class PerformanceTest {
         while (queryDataSet.hasNext()) {
             RowRecord rowRecord = queryDataSet.next();
             count++;
-            if (count % 10000 == 0) {
-                System.out.println("====" + count + "  " + rowRecord);
-            }
+            output(count, rowRecord, true);
         }
 
         long endTime = System.currentTimeMillis();
-        System.out.println(String.format("Time consume : %s, count number : %s", endTime - startTime, count));
+        System.out.println(String.format("start:%s, end:%s. Time consume : %s, count number : %s", deviceStart, deviceEnd, endTime - startTime, count));
+
+    }
+
+    public static void output(int cnt, RowRecord rowRecord, boolean flag) {
+        if (!flag) {
+            return;
+        }
+
+        if (cnt % 10000 == 0) {
+            System.out.println(cnt + " : " + rowRecord);
+        }
+
+        if (cnt > 97600) {
+            System.out.println("----" + cnt + " : " + rowRecord);
+        }
     }
 
     public static Path getPath(int d, int s) {
