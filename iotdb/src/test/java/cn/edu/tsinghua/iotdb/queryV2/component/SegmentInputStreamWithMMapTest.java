69ebad77e233556cba290048dc516f65e2773a40
XuYi <xuyithuss@126.com>
2018-12-25 12:43:02 +0800

    add destory for mappedbytebuffer

--- iotdb/src/test/java/cn/edu/tsinghua/iotdb/queryV2/component/SegmentInputStreamWithMMapTest.java
+++ iotdb/src/test/java/cn/edu/tsinghua/iotdb/queryV2/component/SegmentInputStreamWithMMapTest.java
@@ -2,7 +2,7 @@ package cn.edu.tsinghua.iotdb.queryV2.component;
 
 import cn.edu.tsinghua.iotdb.queryV2.SimpleFileWriter;
 import cn.edu.tsinghua.iotdb.queryV2.engine.reader.component.SegmentInputStreamWithMMap;
-import sun.nio.ch.DirectBuffer;
+import cn.edu.tsinghua.iotdb.utils.CommonUtils;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -14,10 +14,6 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.nio.Buffer;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -47,7 +43,7 @@ public class SegmentInputStreamWithMMapTest {
 
     @Test
     public void testWithMMap() throws Exception {
-    	int javaVersion = getJDKVersion();
+    	int javaVersion = CommonUtils.getJDKVersion();
     	if(javaVersion < 8) {
     		fail(String.format("Current JDK verions is 1.%d, JDK requires 1.8 or later.", javaVersion));
     	}
@@ -57,27 +53,11 @@ public class SegmentInputStreamWithMMapTest {
         testOneSegmentWithMMap(buffer1, 20, 1000);
         testOneSegmentWithMMap(buffer1, 30, 1000);
         testOneSegmentWithMMap(buffer1, 1000, 1000);
-    	if (javaVersion == 8) {
-    		((DirectBuffer) buffer1).cleaner().clean();
-    	} else {
-    		destroyBuffer(buffer1);
-    	}
+    	CommonUtils.destroyBuffer(buffer1);
     	 randomAccessFile.close();
     }
 
-    private void destroyBuffer(Buffer byteBuffer) throws Exception {
-    	try {
-        	final Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-        	final Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
-        	theUnsafeField.setAccessible(true);
-        	final Object theUnsafe = theUnsafeField.get(null);
-        	final Method invokeCleanerMethod = unsafeClass.getMethod("invokeCleaner", ByteBuffer.class);
-        	invokeCleanerMethod.invoke(theUnsafe, byteBuffer);
-		} catch (Exception e) {
-			throw e;
-		}
 
-	}
     
     private void testOneSegmentWithMMap(MappedByteBuffer buffer, int offset, int size) throws IOException {
         SegmentInputStreamWithMMap segmentInputStream = new SegmentInputStreamWithMMap(buffer, offset, size);
@@ -98,15 +78,5 @@ public class SegmentInputStreamWithMMapTest {
         for (int i = startPos; i < len; i++) {
             Assert.assertEquals(bytes[i + offset], ret[i - startPos]);
         }
-    }
-    
-    private int getJDKVersion() {
-    	System.out.println(System.getProperty("java.version"));
-    	String[] javaVersionElements = System.getProperty("java.version").split("\\.");
-    	if(Integer.parseInt(javaVersionElements[0]) == 1) {
-    		return Integer.parseInt(javaVersionElements[1]);
-    	} else {
-    		return Integer.parseInt(javaVersionElements[0]);
-    	}
     }
 }
