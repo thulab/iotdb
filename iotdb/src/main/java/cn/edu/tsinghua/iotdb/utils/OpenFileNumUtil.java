a04a18637e72c00180b5b9134d87ee612bde816a
liuruiyiyang <245422695@qq.com>
2018-12-27 16:43:16 +0800

    trigger ci

--- iotdb/src/main/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtil.java
+++ iotdb/src/main/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtil.java
@@ -49,6 +49,7 @@ public class OpenFileNumUtil {
         public List<String> getPath() {
             return path;
         }
+
         OpenFileNumStatistics(List<String> path){
             this.path = path;
         }
