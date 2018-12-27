2f2dbe71eae9fbb59edfb5d5e2a08b97f984dd67
liuruiyiyang <245422695@qq.com>
2018-12-27 16:06:27 +0800

    trigger ci again

--- iotdb/src/test/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtilTest.java
+++ iotdb/src/test/java/cn/edu/tsinghua/iotdb/utils/OpenFileNumUtilTest.java
@@ -29,7 +29,7 @@ public class OpenFileNumUtilTest {
     private int totalOpenFileNumBefore;
     private int totalOpenFileNumAfter;
     private int totalOpenFileNumChange;
-    private int testFileNum = 88;
+    private int testFileNum = 66;
     private String currDir;
     private File testDataDirRoot;
     private String os = System.getProperty("os.name").toLowerCase();
