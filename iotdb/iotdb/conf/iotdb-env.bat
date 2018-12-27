37993626839481dabbeff018a33f592b15cf7542
myxof <xuyi556677@163.com>
2018-12-27 18:22:23 +0800

    support for jdk 11

--- iotdb/iotdb/conf/iotdb-env.bat
+++ iotdb/iotdb/conf/iotdb-env.bat
@@ -11,16 +11,18 @@ if "%LOCAL_JMX%" == "yes" (
 	)
 
 
+echo %IOTDB_HOME%
+
 IF ["%IOTDB_HEAP_OPTS%"] EQU [""] (
 	rem detect Java 32 or 64 bit
 	java -d64 -version >nul 2>&1
 	IF NOT ERRORLEVEL 1 (
 		rem 64-bit Java
 		echo Detect 64-bit Java, maximum memory allocation pool = 2GB, initial memory allocation pool = 2GB
-		set IOTDB_HEAP_OPTS=-Xmx2G -Xms2G -Xloggc:"%IOTDB_HOME%\gc.log" -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+		set IOTDB_HEAP_OPTS=-Xmx2G -Xms2G  
 	) ELSE (
 		rem 32-bit Java
 		echo Detect 64-bit Java, maximum memory allocation pool = 512MB, initial memory allocation pool = 512MB
-		set IOTDB_HEAP_OPTS=-Xmx512M -Xms512M -Xloggc:"%IOTDB_HOME%\gc.log" -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+		set IOTDB_HEAP_OPTS=-Xmx512M -Xms512M  
 	)
 )
